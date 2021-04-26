162e697c3dbd5f3cadc72419ba826db72c6c2bc3
AraChilingaryan <chilingaryanara96@gmail.com>
2021-04-26 22:58:56 +0300

    Test commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = sc.nextInt();
+        int k = 0;
+        for (int i = 1; k < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print(i + " ");
+                    k++;
+                    if(k == n) break;
+                }
+
+        }
+
+    }
+}
